98a07d7e37aae980fff36720e723d35fa5e603ee
MalcolmTompkins <82959788+Malcolm-Tompkins@users.noreply.github.com>
2022-01-12 17:32:14 +0000

    Prints our 8 memory cards

--- js/game.js
+++ js/game.js
@@ -2,28 +2,17 @@
 
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Malcolm Tompkins
 // Created on: Sep 2020
 // This is the Phaser3 configuration file
 
 //* Game scene */
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: false
-    }
-  },
-  // set background color
-  backgroundColor: 0xffffff,
-  scale: {
-    mode: Phaser.Scale.FIT,
-    // we place it in the middle of the page.
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
-}
-
 const game = new Phaser.Game(config)
+const config = {
+  width: 2000,
+  height: 1000,
+  // set background color
+  backgroundColor: 0xfffff,
+}
+let scene = new SceneMain();
+console.log(game)
